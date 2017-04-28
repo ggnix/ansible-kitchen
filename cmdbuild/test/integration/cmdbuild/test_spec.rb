f647286152f2450c7dc3a41ea7b00cb3f0c9a659
EC2 Default User <ec2-user@ip-172-31-12-164.eu-central-1.compute.internal>
2017-04-28 19:41:08 +0000

    Tomcat+jdk

--- cmdbuild/test/integration/cmdbuild/test_spec.rb
+++ cmdbuild/test/integration/cmdbuild/test_spec.rb
@@ -2,3 +2,11 @@ describe command('java -version') do
   its(:stderr) { should match /1.8.0_05/ }
   its(:exit_status) { should eq 0 }
 end
+
+describe port(8080) do
+  it { should be_listening }
+end
+
+describe file('/opt/tomcat') do
+  it { should be_directory }
+end
